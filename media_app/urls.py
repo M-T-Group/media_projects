afcce8f3bcb1909ac08b364e25eda1ab8e849821
Master-H1 <abawemerchior@gmail.com>
2024-02-13 11:03:51 +0200

    links

--- media_app/urls.py
+++ media_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
-    path("",views.index,name='index'),
+urlpatterns = [
+    path("", views.index, name='index'),
+    path('login/', views.LoginView, name='login'),
+    path('register/', views.RegisterView, name='register')
 ]
